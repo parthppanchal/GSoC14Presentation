1ce0ee6e8a27a34ec2faaa944a3b35740e6ef5cc
Parth Panchal <parthpanchl@gmail.com>
2015-08-20 20:34:46 +0530

    Adds core-service & sync-engine-spec steps

--- js/gsoc-14-presentation.js
+++ js/gsoc-14-presentation.js
@@ -1,4 +1,20 @@
 $( document ).ready(function() {
+    if (screen.width < 500) {
+
+      // :hover will trigger also once the cells are focusable
+      // you can use this class to separate things
+      $("body").addClass("nohover");
+
+      // Make all the cells focusable
+      $("td, th")
+        .attr("tabindex", "1")
+        // When they are tapped, focus them
+        .on("touchstart", function() {
+          $(this).focus();
+        });
+
+    }
+
     function animation(stepId, toFind, animateClass){
         $('#'+stepId).on('impress:stepenter', function(){
             var step = $(this);
